b6926bf84c75070eb54171011e4a65530655333a
Jealousmango <jealousmango@gmail.com>
2017-01-11 06:48:35 -0800

    First Commit
    This will serve as our lord and master.

---
+++ PotionsVRTest/Assets/Scripts/WandController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller {
+        get { return SteamVR_Controller.Input((int)trackedObject.index); }
+    }
+
+    private SteamVR_TrackedObject trackedObject;
+    private GameObject pickup;
+
+	// Use this for initialization
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (controller == null) {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        if (controller.GetPressDown(gripButton) && pickup != null) {
+            pickup.transform.parent = this.transform;
+            pickup.GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        if (controller.GetPressUp(gripButton) && pickup != null) {
+            pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        pickup = collider.gameObject;
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        pickup = null;
+    }
+
+}
